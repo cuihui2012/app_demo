8b5c139055fbaafd6f5273311c81e93fdd4d1c08
cuihui2012 <751670441@qq.com>
2019-11-17 10:57:28 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,7 @@
 ##编译
 cd $PROJ_PATH/app_demo
 mvn clean install -DskipTests
+##war包部署
+docker cp $PROJ_PATH/app_demo/target/app_demo.war 80a011bf441a:/usr/local/tomcat/webapps
+##重启docker容器
+docker restart 80a011bf441a
